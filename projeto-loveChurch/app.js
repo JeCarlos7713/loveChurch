795ff163015673730a36c80691e6e86b575f8d46
Jean Carlos <contato.jeancl25@gmail.com>
2022-01-31 21:19:50 -0300

    Envia construção de cards com Javascript

--- projeto-loveChurch/app.js
+++ projeto-loveChurch/app.js
@@ -1,5 +1,37 @@
-//Efeito Máquina de Escrever
+//Cards
 
+const divCards = document.querySelector("div.cards");
+const card = document.querySelector("div.card");
+
+const content = [
+    {
+        img: "./img/card-teen.jpg",
+        title: "Jovens",
+        id: "1"
+    },
+    {
+        img: "./img/card-woman.jpg",
+        title: "Mulheres",
+        id: "2"
+    },
+    {
+        img: "./img/card-child.jpg",
+        title: "Crianças",
+        id: "3"
+    }
+];
+
+content.map(contain => {
+    const cardClone = card.cloneNode(true);
+    cardClone.setAttribute("id", contain.id);
+    cardClone.querySelector("img").src = contain.img;
+    cardClone.querySelector(".title-departament > h3").innerHTML = contain.title;
+    divCards.appendChild(cardClone);
+});
+
+
+
+//Efeito Máquina de Escrever
 function typeWrite(e) {
     const textArray = e.innerHTML.split('');
     e.innerHTML = '';
@@ -14,3 +46,4 @@ function typeWrite(e) {
 const phrase = document.querySelector('.title');
 
 typeWrite(phrase);
+//Efeito Máquina de Escrever
